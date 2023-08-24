9fb06abb35e98942aedb20ea041b492bff17af55
premiikha_s <spremikhha@gmail.com>
2023-08-25 00:59:16 +0530

    integrated GA

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 import Contact from './Contact/Contact';
 import Settings from './Settings/Settings';
 import Nav from './Nav/Nav';
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+const TRACKING_ID = "UA-282970083-1";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+useEffect(()=>{
+  ReactGA.pageview(window.location.pathname + window.location.search);
+},[])
   return (
     <div className="App">
       <Nav/>
