681819c15843f465f8617b9477f0eececd38ecfa
premiikha_s <spremikhha@gmail.com>
2023-08-25 12:29:45 +0530

    added reactga4

--- src/App.js
+++ src/App.js
@@ -4,15 +4,9 @@ import Home from './Home/Home';
 import Contact from './Contact/Contact';
 import Settings from './Settings/Settings';
 import Nav from './Nav/Nav';
-import ReactGA from 'react-ga';
-import { useEffect } from 'react';
-const TRACKING_ID = "UA-282970083-1";
-ReactGA.initialize(TRACKING_ID);
 
 function App() {
-useEffect(()=>{
-  ReactGA.pageview(window.location.pathname + window.location.search);
-},[])
+
   return (
     <div className="App">
       <Nav/>
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize("G-6LD6THLGTW")
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -13,8 +15,13 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
